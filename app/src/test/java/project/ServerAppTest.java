bfeb5a795c8736dc25aa5f16760baa983d63a446
chocolithm <kys8336@gmail.com>
2024-10-14 17:07:04 +0900

    NCP DB 연결

--- app/src/test/java/project/ServerAppTest.java
+++ app/src/test/java/project/ServerAppTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 class ServerAppTest {
     @Test void appHasAGreeting() {
-        ServerApp classUnderTest = new ServerApp();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
     }
 }
